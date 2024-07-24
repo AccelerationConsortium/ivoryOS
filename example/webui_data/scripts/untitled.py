8e379ea73961f29572f6e467fc0cf8a56940110c
lucyhao <hao.lucyy@gmail.com>
2024-07-24 15:15:16 -0700

    feature: untested using variables as args

--- example/webui_data/scripts/untitled.py
+++ example/webui_data/scripts/untitled.py
@@ -12,5 +12,9 @@ def untitled_script():
 	num = 1
 	my_str = "str"
 	my_bool = True
-	deck.sdl.dilute(**{'factor': 1.0, 'solvent': '"str"'})
-	deck.sdl.dilute(**{'factor': 5.0, 'solvent': '"str"'})
+	test = 12
+	deck.sdl.dilute(**{'factor': test, 'solvent': my_str})
+	deck.sdl.dilute(**{'factor': test, 'solvent': '"another str"'})
+	deck.sdl.dilute(**{'factor': 2.0, 'solvent': '"another str"'})
+	deck.sdl.dose_solvent(**{'amount_in_ml': 5.0, 'name': my_str, 'rate_ml_per_minute': test})
+	deck.sdl.dose_solvent(**{'amount_in_ml': test, 'name': my_str, 'rate_ml_per_minute': num})
