ec882aab516cb105a713d641ff0c45f58d1dfaf0
ivoryzhangwy <ivoryzhangwy@gmail.com>
2024-08-19 15:06:23 -0700

    fix login_view

--- ivoryos/app.py
+++ ivoryos/app.py
@@ -23,7 +23,7 @@ def create_app(config_class=None):
     # Initialize extensions
     socketio.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = "login"
+    login_manager.login_view = "auth.login"
     db.init_app(app)
 
     # Create database tables
