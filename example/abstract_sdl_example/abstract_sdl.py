7b72877e725c4ebef7cbd78542d43e17eeb59df0
ivoryzhangwy <ivoryzhangwy@gmail.com>
2025-04-17 15:39:07 -0700

    clean up

--- example/abstract_sdl_example/abstract_sdl.py
+++ example/abstract_sdl_example/abstract_sdl.py
@@ -12,6 +12,7 @@ import os
 import sys
 import time
 from abc import ABC
+import random
 from typing import List, Union
 
 from example.abstract_sdl_example.abstract_balance import AbstractBalance
@@ -24,14 +25,17 @@ class AbstractSDL(ABC):
         self.balance = balance
         self.logger = logging.getLogger(f"logger_name")
 
-    def dose_solvent(self, solvent_name: str, amount_in_ml: float = 5, rate_ml_per_minute: float = 1):
-        self.pump.dose_liquid(amount_in_ml=amount_in_ml, rate_ml_per_minute=rate_ml_per_minute)
-        self.balance.weigh_sample()
-        self.logger.info(f"dosing {solvent_name} solvent")
-        time.sleep(1)
-        return 1
+    def analyze(self,
+                param_1:int,
+                param_2:int):
+        self.logger.info("analyze")
+        return random.randint(param_1, param_2)
 
-    def dose_solid(self, amount_in_mg: float = 5, bring_in: bool = False):
+
+    def dose_solid(self,
+                   amount_in_mg: float = 5,
+                   bring_in: bool = False
+                   ):
         """dose current chemical"""
         self.balance.dose_solid(amount_in_mg=amount_in_mg)
         self.balance.weigh_sample()
@@ -40,41 +44,27 @@ class AbstractSDL(ABC):
         self.logger.info(f"dosing solid {amount_in_mg}, bring in {bring_in}")
         return 1
 
-    def equilibrate(self, temp: float, duration: float):
+    def dose_solvent(self,
+                     solvent_name: str,
+                     amount_in_ml: float = 5,
+                     rate_ml_per_minute: float = 1
+                     ):
+        self.pump.dose_liquid(amount_in_ml=amount_in_ml, rate_ml_per_minute=rate_ml_per_minute)
+        self.balance.weigh_sample()
+        self.logger.info(f"dosing {solvent_name} solvent")
+        time.sleep(1)
+        return 1
+
+
+    def equilibrate(self,
+                    temp: float,
+                    duration: float
+                    ):
         self.logger.info(f"equilibrate at {temp} for {duration}")
-
-    def analyze(self):
-        self.logger.info("analyze")
-        return 3
-
-    def filtration(self, test):
-        for i in test:
-            print(i)
-        self.logger.info("filtration")
 
     def _send_command(self):
         """helper function"""
         pass
-
-    def example_function(self,
-                         a: int,
-                         b: str,
-                         # c: float | None,  # Optional[float]
-                         # d: str | int,  # Union[str, int]
-                         e: list[str],  # List[str]
-                         f: dict[str, int],  # Dict[str, int]
-                         g: tuple[int, str, float]  # Tuple[int, str, float]
-                         ):
-
-        pass
-
-
-    def prepare_extraction_vial(self,
-                                 crude_plate: str = 'extraction', crude_volume: Union[float, int] = 3000,
-                                 org_solvent: str = "dichloromethane", org_volume: Union[float, int] = 2000,
-                                 aq_solvent: str = "water", aq_volume: int = 2000):
-
-        print(type(aq_volume))
 
 
 # some constant values, non-module variables are currently not extracted to UI
@@ -91,7 +81,7 @@ if __name__ == "__main__":
     import ivoryos
 
     # USE CASE 1 - start OS using current module
-    # ivoryos.run(__name__)
+    ivoryos.run(__name__)
 
     # # USE CASE 2 - start OS using current module and enable LLM with Ollama
     # ivoryos.run(__name__, model="llama3.1", llm_server='localhost')
@@ -103,7 +93,7 @@ if __name__ == "__main__":
     # ivoryos.run()
 
     # # LOGGER EXAMPLE - start OS using current module and include logger
-    ivoryos.run(__name__, logger='logger_name')
+    # ivoryos.run(__name__, logger='logger_name')
 
     """
     Example prompt when using text-to-code:
