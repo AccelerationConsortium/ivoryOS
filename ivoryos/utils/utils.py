79e682f6fb662f3a6b9a11eff4059bccc27c16d4
ivoryzhang <ivoryzhangwy@gmail.com>
2025-07-03 02:39:56 -0700

    python version compatability

--- ivoryos/utils/utils.py
+++ ivoryos/utils/utils.py
@@ -407,3 +407,15 @@ def get_method_from_workflow(function_string, func_name="workflow"):
 #         method = get_method_from_workflow(compiled_strs, func_name=workflow.name)
 #         setattr(RegisteredWorkflows, workflow.name, staticmethod(method))
 #     global_config.registered_workflows = RegisteredWorkflows()
+
+
+def get_local_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('10.255.255.255', 1))  # Dummy address to get interface IP
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
