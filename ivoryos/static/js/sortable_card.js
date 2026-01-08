7d22f2ec260bfe3d28ae072b54b98e993d8310e6
ivoryzhangwy <ivoryzhangwy@gmail.com>
2026-01-07 18:34:27 -0800

    format config page and controllers.html ui

--- ivoryos/static/js/sortable_card.js
+++ ivoryos/static/js/sortable_card.js
@@ -1,24 +1,25 @@
-$(function() {
+$(function () {
     $("#sortable-grid").sortable({
-        items: ".card",
+        items: ".draggable-method-card",
         cursor: "move",
+        cancel: "input,textarea,button,select,option,a,[data-bs-toggle='popover']",
         opacity: 0.7,
         revert: true,
         placeholder: "ui-state-highlight",
-        start: function(event, ui) {
+        start: function (event, ui) {
             ui.placeholder.height(ui.item.height());
         },
-        update: function() {
+        update: function () {
             const newOrder = $("#sortable-grid").sortable("toArray");
             $.ajax({
                 url: saveOrderUrl,  // saveOrderUrl should be set dynamically in your HTML template
                 method: 'POST',
                 contentType: 'application/json',
                 data: JSON.stringify({ order: newOrder }),
-                success: function() {
+                success: function () {
                     console.log('Order saved');
                 }
             });
         }
-    }).disableSelection();
+    });
 });
