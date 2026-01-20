6fc27cc4f8ed19a608ec50503df2e795ef5ccacc
ivoryzhangwy <ivoryzhangwy@gmail.com>
2026-01-19 17:50:27 -0800

    1.5.8 - serialize * to JSON

--- ivoryos/utils/utils.py
+++ ivoryos/utils/utils.py
@@ -542,14 +542,31 @@ def get_local_ip():
     return ip
 
 
-def safe_dump(obj):
+def sanitize_for_json(obj):
+    """
+    Recursively converts sets and other non-JSON-serializable objects to JSON-friendly types.
+    """
+    from datetime import datetime, date
+    
+    if isinstance(obj, dict):
+        return {k: sanitize_for_json(v) for k, v in obj.items()}
+    elif isinstance(obj, (list, tuple)):
+        return [sanitize_for_json(x) for x in obj]
+    elif isinstance(obj, set):
+        return [sanitize_for_json(x) for x in list(obj)]
+    elif isinstance(obj, Enum):
+        return obj.value
+    elif isinstance(obj, (datetime, date)):
+        return obj.isoformat()
     try:
         json.dumps(obj)
         return obj
     except (TypeError, OverflowError):
-        if isinstance(obj, Enum):
-            return obj.value
-        return repr(obj)  # store readable representation
+        return repr(obj)
+
+def safe_dump(obj):
+    return sanitize_for_json(obj)
+
 
 
 def create_module_snapshot(module):
